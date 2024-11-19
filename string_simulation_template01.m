28779d55374caf534f2f0c3c02af11a1dae9128c
Vaani Bhatnagar <98102364+vaanibhatnagar@users.noreply.github.com>
2024-11-19 13:52:25 -0500

    animation works

--- string_simulation_template01.m
+++ string_simulation_template01.m
@@ -29,6 +29,26 @@ function string_simulation_template01()
     tspan = linspace(0,10,200);
     %run the integration
     my_rate_func = @(t0,V0) string_rate_func01(0,V0,string_params);
-    [tlist,Vlist] = ode45(my_rate_func,tspan,V0);
+    [tlist,Vlist] = ode45(my_rate_func,tspan,V0)
     %your code to generate an animation of the system
+    figure(1);
+    for i=1:length(Vlist)
+        clf
+        hold on
+        axis([0, 4, -1.5, 1.5])
+
+        plot([xlist(1),xlist(2)], [0, Vlist(i,1)], 'b-', 'LineWidth', 1);
+        plot([xlist(2),xlist(3)], [Vlist(i,1), Vlist(i,2)], 'b-', 'LineWidth', 1);
+        plot([xlist(3),xlist(4)], [Vlist(i,2), Vlist(i,3)], 'b-', 'LineWidth', 1);
+        plot([xlist(4),xlist(5)], [Vlist(i,3), Uf_func(tlist(i))], 'b-', 'LineWidth', 1);
+        plot(xlist(1),0, "bo")
+        plot(xlist(2),Vlist(i,1), "r.", 'MarkerSize', 50)
+        plot(xlist(3),Vlist(i,2), "r.", 'MarkerSize', 50)
+        plot(xlist(4),Vlist(i,3), "r.", 'MarkerSize', 50)
+        plot(xlist(5),Uf_func(tlist(i)), "bo")
+        Uf_func(tlist(i))
+        drawnow;
+    end
+
+
 end
