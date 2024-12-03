e2e9a844087080b3e254264798852aa3a84888b3
Vaani Bhatnagar <98102364+vaanibhatnagar@users.noreply.github.com>
2024-12-03 11:54:34 -0500

    subplots

--- string_simulation_template01.m
+++ string_simulation_template01.m
@@ -1,6 +1,6 @@
 function string_simulation_template01()
     
-    mode_shape = 2;
+    mode_shape = 5;
     num_masses = 3;
     total_mass = 1;
     tension_force = 30;
@@ -38,8 +38,13 @@ function string_simulation_template01()
     string_params.Uf_func = Uf_func;
     string_params.dUfdt_func = dUfdt_func;
 
-    figure(1)
-    plot(xlist, Ur_mat(mode_shape))
+    subplot(2,1,1)
+    hold on
+    ydata = Ur_mat(:,mode_shape);
+    plot(xlist,ydata,'r','LineWidth',2);
+    plot(xlist(2:end-1),ydata(2:end-1),'ko','MarkerFaceColor','k','MarkerSize',5);
+    plot([xlist(1),xlist(end)],[ydata(1),ydata(end)],'bo','MarkerFaceColor','b','MarkerSize',5);
+    hold off
 
     %run the integration
     my_rate_func = @(t_in,V_in) string_rate_func01(t_in,V_in,string_params);
@@ -48,8 +53,7 @@ function string_simulation_template01()
 
 
     %your code to generate an animation of the system
-    figure(1);
-
+    subplot(2,1,2)
     hold on
     axis([0, 4, -1.5, 1.5])
 
@@ -64,8 +68,9 @@ function string_simulation_template01()
         set(plot1,'xdata',xlist,'ydata',Uplot);
         set(plot2,'xdata',xlist(2:end-1),'ydata',Uplot(2:end-1));
         set(plot3,'xdata',[xlist(1),xlist(end)],'ydata',[Uplot(1),Uplot(end)]);
-
+        
         drawnow;
     end
+    hold off
 
 end
